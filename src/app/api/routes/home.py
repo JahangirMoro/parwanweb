3da13ce3dde83d843cdedab23f1348e95cf6ff8a
JahangirMoro <jahangeerrathore@gmail.com>
2025-07-25 07:48:45 +0500

    ✅ Fix ImportError: added Epaper model

--- src/app/api/routes/home.py
+++ src/app/api/routes/home.py
@@ -6,9 +6,16 @@ from app.models.news import NewsArticle
 from fastapi.responses import HTMLResponse
 from app.models.category import Category
 from datetime import datetime
+from fastapi.templating import Jinja2Templates
+import os
 
 router = APIRouter()
-templates = Jinja2Templates(directory="src/app/templates")
+#templates = Jinja2Templates(directory="src/app/templates")
+# Calculate absolute path to templates folder
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+TEMPLATES_DIR = os.path.join(BASE_DIR, "app", "templates")
+
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 @router.get("/", response_class=HTMLResponse)
 def homepage(request: Request, db: Session = Depends(get_db)):
